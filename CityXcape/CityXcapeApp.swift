9e7bccfac3087110352d40983add08b73c8e1b3b
James Allan <James@cityXcape.com>
2021-08-17 20:45:01 -0400

    Initial Commit

---
+++ CityXcape/CityXcapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityXcapeApp.swift
+//  CityXcape
+//
+//  Created by James Allan on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct CityXcapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityXcape/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CityXcape
+//
+//  Created by James Allan on 8/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
